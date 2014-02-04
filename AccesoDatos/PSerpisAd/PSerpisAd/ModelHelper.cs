060586d16dba952ad449d01effd42be210cbfa01
javierlmserpis <javierlmserpis@gmail.com>
2014-02-04 14:42:28 +0100

    04-02-14

--- AccesoDatos/PSerpisAd/PSerpisAd/ModelHelper.cs
+++ AccesoDatos/PSerpisAd/PSerpisAd/ModelHelper.cs
@@ -1,12 +1,64 @@
 using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Reflection;
 
-namespace SerpisAd
+namespace Serpis.Ad
 {
 	public class ModelHelper
 	{
-		public ModelHelper ()
-		{
+		public static string GetSelect(Type type) {
+			string keyName = null;
+			List<string> fieldNames = new List<string>();
+			foreach (PropertyInfo propertyInfo in type.GetProperties ()) {
+				if (propertyInfo.IsDefined (typeof(KeyAttribute), true))
+					keyName = propertyInfo.Name.ToLower ();
+				else if (propertyInfo.IsDefined (typeof(FieldAttribute), true))
+					fieldNames.Add (propertyInfo.Name.ToLower());
+			}
+			
+			string tableName = type.Name.ToLower();
+			
+			return string.Format ("select {0} from {1} where {2}=",
+			                      string.Join(", ", fieldNames), tableName, keyName);
 		}
+		
+		public static object Load(Type type, string id) {
+			IDbCommand selectDbCommand = App.Instance.DbConnection.CreateCommand ();
+			selectDbCommand.CommandText = GetSelect(type) + id;
+			IDataReader dataReader = selectDbCommand.ExecuteReader();
+			dataReader.Read(); //lee el primero
+			
+			object obj = Activator.CreateInstance(type);
+			foreach (PropertyInfo propertyInfo in type.GetProperties ()) {
+				if (propertyInfo.IsDefined (typeof(KeyAttribute), true))
+					propertyInfo.SetValue(obj, id, null); //TODO convert al tipo destino
+					
+				else if (propertyInfo.IsDefined (typeof(FieldAttribute), true))
+					propertyInfo.SetValue(obj, dataReader[propertyInfo.Name.ToLower()], null); //TODO convert al tipo de destino
+			}
+			dataReader.Close ();
+			return obj;
+		}
+		
+		public static object Save(Type type, string id){
+			IDbCommand saveDbCommand = App.Instance.DbConnection.CreateCommand ();
+			saveDbCommand.CommandText = GetSelect(type) + id;
+			IDataReader dataReader = updateDbCommand.ExecuteReader();
+			dataReader.Read(); //lee el primero
+			
+			object obj = Activator.CreateInstance(type);
+			foreach (PropertyInfo propertyInfo in type.GetProperties ()) {
+				if (propertyInfo.IsDefined (typeof(KeyAttribute), true)){
+					propertyInfo.SetValue(obj, id, null); //TODO convert al tipo destino
+					updateDbCommand.CommandText = "update categoria set nombre=@nombre where id=" + categoria.Id;
+				}else if (propertyInfo.IsDefined (typeof(FieldAttribute), true))
+					propertyInfo.SetValue(obj, dataReader[propertyInfo.Name.ToLower()], null); //TODO convert al tipo de destino
+			}
+			dataReader.Close ();
+			return obj;
+		}
+		
 	}
 }
 
